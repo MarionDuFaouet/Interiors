b1ad82c8a3fb6a13b70d4d67907b5f71b61ea0f2
MarionDuFaouet <lozachm@gmail.com>
2024-01-23 18:36:55 +0100

    confiture de fetch et bébé excerpt

--- news.js
+++ news.js
@@ -1,16 +1,10 @@
 // ARTICLE 1
 
-// ma promesse
+// requête XHR
 let ajax = new Promise((resolve, reject) => {
-    // Création de l'objet XMLHttpRequest
-    // XMLHttpRequest est une api!!! Appelée plus couramment XHR
     let request = new XMLHttpRequest();
-    //initialisation requête.
     request.open("GET", "https://www.tbads.eu/greta/kercode/ajax/?article=1");
-    //répond en format json
     request.responseType = "json";
-    //ajoute un écouteur d'événement à request
-    //,vérifie et...
     request.addEventListener("load", function () {
         if (request.status === 200) {
             resolve(request.response);
@@ -21,15 +15,12 @@ let ajax = new Promise((resolve, reject) => {
     request.addEventListener("error", function () {
         reject("La requête ajax a échoué");
     }, false);
-    // ...envoie la requête
     request.send();
 });
 
-
-//la méthode then est déclenchée par le résultat de vérif de ma fonction (if ou else)
 ajax.then(function (response) {
     console.log("Response : %o", response);
-    //appel de ma fonction
+    // mes fonctions
     contenu(response);
     createExcerpt(response);
 })
@@ -37,28 +28,34 @@ ajax.then(function (response) {
         console.log("Message d'erreur :", msg)
     });
 
+//récupère mes éléments html et injecte ls données json
 function contenu(jsonObj) {
-    //récupère mes éléments html
     let myTitle = document.querySelector("#title");
     let myDateNews = document.querySelector("#dateNews");
     let myDateModale = document.querySelector("#dateModale");
-    // let myContentNews = document.querySelector("#contentNews");
     let myContentModale = document.querySelector("#contentModale");
     let myPicture = document.querySelector("#picture");
 
-
-    // injection json
     myTitle.textContent = jsonObj.title;
     myDateNews.textContent = jsonObj.date.day + ' ' + jsonObj.date.month + ' ' + jsonObj.date.year;
     myDateModale.textContent = jsonObj.date.day + ' ' + jsonObj.date.month + ' ' + jsonObj.date.year;
-    // myContentNews.textContent = jsonObj["content"][0];
     myContentModale.textContent = jsonObj.content;
     myPicture.setAttribute("src", jsonObj.picture);
-
 }
+
+// Création excerpt
+function createExcerpt(jsonObj) {
+    let myExcerpt = document.querySelector("#contentNews");
+    let text = jsonObj.content[0];
+    text = text.substring(0, 60) + " ...";
+    myExcerpt.textContent = text;
+}
+
 // ==============================================================================================================
 
 // ARTICLE 2
+
+// méthode fetch
 fetch("https://www.tbads.eu/greta/kercode/ajax/?article=3")
     .then(response => response.json())
     .then(function (response) {
@@ -66,7 +63,6 @@ fetch("https://www.tbads.eu/greta/kercode/ajax/?article=3")
         //mes fonctions;
         contenuBis(response);
         createExcerptBis(response);
-        // countWords(response);
     })
     .catch(function (msg) {
         console.log("Message d'erreur:", msg)
@@ -78,28 +74,17 @@ function contenuBis(jsonObj) {
     let myTitleBis = document.querySelector("#titleBis");
     let myDateNewsBis = document.querySelector("#dateNewsBis");
     let myDateModaleBis = document.querySelector("#dateModaleBis");
-    // let myContentNewsBis = document.querySelector("#contentNewsBis");
     let myContentModaleBis = document.querySelector("#contentModaleBis");
     let myPictureBis = document.querySelector("#pictureBis");
 
-    // injection json
     myTitleBis.textContent = jsonObj.title;
     myDateNewsBis.textContent = jsonObj.date.day + ' ' + jsonObj.date.month + ' ' + jsonObj.date.year;
     myDateModaleBis.textContent = jsonObj.date.day + ' ' + jsonObj.date.month + ' ' + jsonObj.date.year;
-    // myContentNewsBis.textContent = jsonObj["content"][0];
     myContentModaleBis.textContent = jsonObj.content;
     myPictureBis.setAttribute("src", jsonObj.picture);
 }
-//=========================================================================================================================
+
 // Création excerpt
-function createExcerpt(jsonObj) {
-    let myExcerpt = document.querySelector("#contentNews");
-    let text = jsonObj.content[0];
-    text = text.substring(0, 60) + " ...";
-    myExcerpt.textContent = text;
-}
-
-
 function createExcerptBis(jsonObj) {
     let myExcerptBis = document.querySelector("#contentNewsBis");
     let text = jsonObj.content[0];
@@ -107,49 +92,27 @@ function createExcerptBis(jsonObj) {
     myExcerptBis.textContent = text;
 }
 
-// // Compte le nombre d'espace, et quand j'arrive à 20, 
-// function countWords(jsonObj) {
-//     let sentence = jsonObj["content"][0];
-//     let text = sentence.substring(0, 60);
-//     let space = ' ';
-//     let count = 0;
-//     let myExcerptBis = [];
-
-//     for (let i = 0; i < sentence.length; i++) {
-//         let currentChar = text.charAt(i);
-
-//         if (currentChar === space)
-//             count++;
-//         myExcerptBis.push(sentence)
-//     }
-//     console.log(`nombre de mot ${count}`);
-// }
-
-// // trouve le dernier espace
-// function lastSpace(jsonObj) {
-//     let sentence = jsonObj["content"][0];
-//     let spaceCount = 0;
-//     let myExcerpt = [];
-
-//     for (let i = 0; i <10; i++) {
-//         let currentChar = sentence.charAt(i);
-//         if (currentChar === ' '){
-//         myExcerpt.push(sentence.substring(0, currentChar));
-//         continue;
-//         }
-//             spaceCount++;
-//     }
-//     if (spaceCount === 10){
-    // myExcerpt.push(sentence.substring(0, ))
-//         return;
-//     }
-// }
-
 // ==============================================================================================================
 
 
 
 // //METHODE KEVIN
+
+// // créé un tableau id
+// let id = [1,3];
+// // Envoie une requête XHR pour chaque indice de id, id définissant l'url de ma requête
+// for (let i = 0; i < id.length; i++) {
+//     let promise= UrlXmlHttpRequest(id[i]);
+//     promise.then(
+//         function(response){
+//             console.log("Response: %o", response);
+//            contenu(response);
+//         }
+//     ).catch(function (msg) {
+//         console.log("Message d'erreur :", msg)
+//     });
+// } 
+
 // // créé une requête XHR pour chaque url
 // function UrlXmlHttpRequest(id) {
 //     return new Promise((resolve, reject) => {
@@ -200,18 +163,3 @@ function createExcerptBis(jsonObj) {
 //         myPicture[i].setAttribute("src") = jsonObj["picture"];
 //     }
 // }
-
-// // créé un tableau id
-// let id = [1,3];
-// // Envoie une requête XHR pour chaque indice de id, id définissant l'url de ma requête
-// for (let i = 0; i < id.length; i++) {
-//     let promise= UrlXmlHttpRequest(id[i]);
-//     promise.then(
-//         function(response){
-//             console.log("Response: %o", response);
-//            contenu(response);
-//         }
-//     ).catch(function (msg) {
-//         console.log("Message d'erreur :", msg)
-//     });
-// } 
